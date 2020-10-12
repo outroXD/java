d54b0c877b67f2b4f29080fb1493122a185ffe48
outroXD <keisuke.nakasuga+githubXD@gmail.com>
2020-10-13 01:31:38 +0900

    読書メモ Java Puzzlers。

---
+++ books/java-puzzlers/src/chap5/Puzzle44.java
@@ -0,0 +1,18 @@
+package chap5;
+
+import common.AbstractPuzzle;
+
+public class Puzzle44 extends AbstractPuzzle {
+
+    public Puzzle44() {
+        super(new Object(){}.getClass().getEnclosingClass().getName());
+    }
+
+    @Override
+    public void showExample() {
+    }
+
+    @Override
+    public void showCorrect() {
+    }
+}
